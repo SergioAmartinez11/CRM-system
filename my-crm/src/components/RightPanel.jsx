dfc97191382a7a6cdd14f84502a3068b0431ac87
Sergio A. Martinez <71477549+SergioAmartinez11@users.noreply.github.com>
2023-03-24 20:30:32 -0700

    Left panel
    Se agrego el panel de la izquierda con botones

---
+++ my-crm/src/components/RightPanel.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/system';
+import React from 'react';
+
+export const RightPanel = () => {
+  return (
+    <>
+      <Container
+        sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}
+      ></Container>
+    </>
+  );
+};
---
+++ my-crm/src/constants/buttons.js
@@ -0,0 +1,8 @@
+export const mainPanelButtons = [
+    { value: 'opt1', label: 'Ventas' },
+    { value: 'opt2', label: 'Marketing'},
+    { value: 'opt3', label: 'Inventario'},
+    { value: 'opt4', label: 'Administrador'},
+    { value: 'opt5', label: 'Configuracion'},
+  ];
+  
