78daba58c9b5bf945f52c8ff0c49ac990c9a7eb5
Sergio A. Martinez <71477549+SergioAmartinez11@users.noreply.github.com>
2023-04-12 20:39:27 -0700

    Se hizo la conexion al backend local
    Se realizo la conexion al backend de forma local.

--- my-crm/src/components/RightPanel.jsx
+++ my-crm/src/components/RightPanel.jsx
@@ -1,18 +1,12 @@
 import { Container } from '@mui/system';
 import React from 'react';
-import Dashboard from './Dashboard';
-import Forms from './Forms';
 
 export const RightPanel = () => {
   return (
     <>
       <Container
         sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}
-      >
-
-      <Dashboard></Dashboard>
-      <Forms/>
-      </Container>
+      ></Container>
     </>
   );
 };
