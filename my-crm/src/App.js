7a8113456db0c45fa3b490b952fe8203c8a19b00
66castrejon <72263190+66castrejon@users.noreply.github.com>
2023-04-22 01:29:23 -0700

    packages

--- my-crm/src/App.js
+++ my-crm/src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 import { Grid } from '@mui/material';
 import { LeftPanel } from './components/LeftPanel';
 import { EmployeeForm } from './components/employees/EmployeeForm';
+import Saludo from './components/Saludo';
 
 
 function App() {
   return (
-    // <Login/>
+    // <Login/> <EmployeeForm />
       <Grid container spacing={0} xs={12} minHeight={'100vh'}>
         <Grid item xs={3}>
-          <LeftPanel/>
+        <LeftPanel/>
         </Grid>
         <Grid item xs={9} sx={{backgroundColor:'#F2EFFF'}} padding={'4rem'}>
-          <EmployeeForm />
+        <Saludo />
         </Grid>
       </Grid>
     
---
+++ my-crm/src/components/Saludo.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', "Refrigerate", 6.0, 0, "Out of Stock"),
+  createData('Ice cream sandwich', "Refrigerate", 9.0, 37, "In Stock"),
+  createData('Orange Juice', "Refrigerate", 16.0, 24, "In Stock"),
+  createData('Bread', "Bakery", 3.7, 0, "Out of Stock"),
+  createData('Cereal', "Other", 16.0, 49, "In Stock"),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function CustomizedTables() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Product</StyledTableCell>
+            <StyledTableCell align="right">Category</StyledTableCell>
+            <StyledTableCell align="right">Price</StyledTableCell>
+            <StyledTableCell align="right">Quantity</StyledTableCell>
+            <StyledTableCell align="right">Status</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
