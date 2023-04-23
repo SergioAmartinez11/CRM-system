52200d4d610a0dca11ceab28339d02c8b2851812
66castrejon <72263190+66castrejon@users.noreply.github.com>
2023-04-22 18:58:22 -0700

    invetary_table

--- my-crm/src/components/Saludo.jsx
+++ my-crm/src/components/Saludo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,6 +7,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -26,53 +28,131 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', "Refrigerate", 6.0, 0, "Out of Stock"),
-  createData('Ice cream sandwich', "Refrigerate", 9.0, 37, "In Stock"),
-  createData('Orange Juice', "Refrigerate", 16.0, 24, "In Stock"),
-  createData('Bread', "Bakery", 3.7, 0, "Out of Stock"),
-  createData('Cereal', "Other", 16.0, 49, "In Stock"),
-];
-
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: 400,
+    marginBottom: '1rem',
+  },
+  input: {
+    marginBottom: '0.5rem',
   },
 });
 
 export default function CustomizedTables() {
   const classes = useStyles();
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const category = form.category.value;
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const status = form.status.value;
+    const newProduct = createData(name, category, price, quantity, status);
+    setProducts([...products, newProduct]);
+    form.reset();
+  };
+
+  const handleDeleteProduct = (name) => {
+    const updatedProducts = products.filter((product) => product.name !== name);
+    setProducts(updatedProducts);
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Product</StyledTableCell>
-            <StyledTableCell align="right">Category</StyledTableCell>
-            <StyledTableCell align="right">Price</StyledTableCell>
-            <StyledTableCell align="right">Quantity</StyledTableCell>
-            <StyledTableCell align="right">Status</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    <>
+      <form onSubmit={handleAddProduct} className={classes.form}>
+        <TextField
+          name="name"
+          label="Product Name"
+          variant="outlined"
+          className={classes.input}
+          required
+        />
+        <TextField
+          name="category"
+          label="Category"
+          variant="outlined"
+          className={classes.input}
+          required
+        />
+        <TextField
+          name="price"
+          label="Price"
+          variant="outlined"
+          type="number"
+          className={classes.input}
+          required
+        />
+        <TextField
+          name="quantity"
+          label="Quantity"
+          variant="outlined"
+          type="number"
+          className={classes.input}
+          required
+        />
+        <TextField
+          name="status"
+          label="Status"
+          variant="outlined"
+          className={classes.input}
+          required
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Add Product
+        </Button>
+      </form>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+
+            <TableRow>
+              <StyledTableCell>Product Name</StyledTableCell>
+              <StyledTableCell align="right">Category</StyledTableCell>
+              <StyledTableCell align="right">Price</StyledTableCell>
+              <StyledTableCell align="right">Quantity</StyledTableCell>
+              <StyledTableCell align="right">Status</StyledTableCell>
+              <StyledTableCell align="right">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+
+      <TableBody>
+        {products.map((product) => (
+          <StyledTableRow key={product.name}>
+            <StyledTableCell component="th" scope="row">
+              {product.name}
+            </StyledTableCell>
+            <StyledTableCell align="right">{product.category}</StyledTableCell>
+            <StyledTableCell align="right">{product.price}</StyledTableCell>
+            <StyledTableCell align="right">{product.quantity}</StyledTableCell>
+            <StyledTableCell align="right">{product.status}</StyledTableCell>
+            <StyledTableCell align="right">
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => handleDeleteProduct(product.name)}
+              >
+                Delete
+              </Button>
+            </StyledTableCell>
+          </StyledTableRow>
+        ))}
+      </TableBody>
+    </Table>
+
+  </TableContainer>
+</>
+);
 }
+
+function createData(name, category, price, quantity, status) {
+return { name, category, price, quantity, status };
+}
+
